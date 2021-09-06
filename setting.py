1b38bfefd9de2719e48d67e50bd487f11e4567a8
yuseonho <youseonho@naver.com>
2021-09-07 08:39:16 +0900

    불필요한 변수 및 함수 삭제

--- setting.py
+++ setting.py
@@ -8,46 +8,17 @@ db_backtest = f'{db_path}/backtest.db'
 qfont12 = QFont()
 qfont12.setFamily('나눔고딕')
 qfont12.setPixelSize(12)
-qfont13 = QFont()
-qfont13.setFamily('나눔고딕')
-qfont13.setPixelSize(13)
-qfont14 = QFont()
-qfont14.setFamily('나눔고딕')
-qfont14.setPixelSize(14)
 
 color_fg_bt = QColor(230, 230, 235)
 color_fg_bc = QColor(190, 190, 195)
 color_fg_dk = QColor(150, 150, 155)
 color_fg_bk = QColor(110, 110, 115)
 
-color_bg_bt = QColor(50, 50, 55)
 color_bg_bc = QColor(40, 40, 45)
 color_bg_dk = QColor(30, 30, 35)
 color_bg_bk = QColor(20, 20, 25)
-color_bg_ld = (50, 50, 55, 150)
 
-color_bf_bt = QColor(110, 110, 115)
-color_bf_dk = QColor(70, 70, 75)
-
-color_cifl = QColor(230, 230, 255)
-color_pluss = QColor(230, 230, 235)
-color_minus = QColor(120, 120, 125)
-
-color_chuse1 = QColor(35, 35, 40)
-color_chuse2 = QColor(30, 30, 35)
-color_ema05 = QColor(230, 230, 235)
-color_ema10 = QColor(200, 200, 205)
-color_ema20 = QColor(170, 170, 175)
-color_ema40 = QColor(140, 140, 145)
-color_ema60 = QColor(110, 110, 115)
-color_ema120 = QColor(80, 80, 85)
-color_ema240 = QColor(70, 70, 75)
-color_ema480 = QColor(60, 60, 65)
-
-style_fc_bt = 'color: rgb(230, 230, 235);'
-style_fc_dk = 'color: rgb(150, 150, 155);'
 style_bc_bt = 'background-color: rgb(50, 50, 55);'
-style_bc_dk = 'background-color: rgb(30, 30, 35);'
 
 ui_num = {'거래합계': 1, '거래목록': 2, '잔고평가': 3, '잔고목록': 4, '체결목록': 5, '관심종목': 6,
           '당일합계': 7, '당일상세': 8, '누적합계': 9, '누적상세': 10}
